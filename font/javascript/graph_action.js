8222b12dc4d142828c2755a4a44c346911f613c0
mahaoxiang822 <464365439@qq.com>
2019-04-09 12:33:44 +0800

    根据需求说明书，重写了前端的基本框架 （1）实现了fix部分 （2）添加了header （3）实现了点击设置属性

--- font/javascript/graph_action.js
+++ font/javascript/graph_action.js
@@ -1,6 +1,7 @@
 var gobalConfig = {
     "base_url": "http://127.0.0.1:8000/"
 };
+
 /*$(function () {
     $('#submit').click( function (e) {
         var conn_list;
@@ -30,27 +31,29 @@ var gobalConfig = {
 });*/
 function save_network() {
     var conn_list;
-        var network = [];
-        conn_list = jsPlumb.getAllConnections();
-        console.log(conn_list);
-        for (var i = 0; i < conn_list.length; i++) {
-            var source_id = conn_list[i]["sourceId"];
-            var target_id = conn_list[i]["targetId"];
-            var conn = {
-                "source": $("#" + source_id).attr("name"),
-                "target": $("#" + target_id).attr("name")
-            }
-            network.push(conn);
+    var network = [];
+    conn_list = jsPlumb.getAllConnections();
+    console.log(conn_list);
+    for (var i = 0; i < conn_list.length; i++) {
+        var source_id = conn_list[i]["sourceId"];
+        var target_id = conn_list[i]["targetId"];
+        var conn = {
+            "source": $("#" + source_id).attr("name"),
+            "target": $("#" + target_id).attr("name")
         }
+        network.push(conn);
+    }
 
-       $.ajax({
-            type: 'POST',
-            url: gobalConfig.base_url + 'NeuralNetwork/network/',
-            data: JSON.stringify(network),
-            contentType: 'application/json; charset=UTF-8',
-            success: function (data_return) {
-                alert(data_return);
-            }
-        });
+    $.ajax({
+        type: 'POST',
+        url: gobalConfig.base_url + 'NeuralNetwork/network/',
+        data: JSON.stringify(network),
+        contentType: 'application/json; charset=UTF-8',
+        success: function (data_return) {
+            alert(data_return);
+        }
+    });
 }
+
+
 
