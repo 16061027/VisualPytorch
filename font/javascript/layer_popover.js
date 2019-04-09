f4bbe31fc3fdafa570516a8f598d92ebde5c470e
mahaoxiang822 <464365439@qq.com>
2019-04-09 16:41:24 +0800

    重构节点属性设置代码

---
+++ font/javascript/layer_popover.js
@@ -0,0 +1,23 @@
+var view_layer = "<form class=\"form-horizontal\" role=\"form\"><div class=\"form-group\">" +
+    "                    <label class=\"col-sm-5 control-label\">shape</label>" +
+    "                            <div class=\"col-sm-5\">" +
+    "                        <input type=\"text\"; class=\"form-control\" placeholder=\"1\">" +
+    "                    </div>" +
+    "                </div>" +
+    "                </form>"
+var linear_layer = "<form class=\"form-horizontal\" role=\"form\"><div class=\"form-group\">" +
+    "                    <label class=\"col-sm-5 control-label\">in_channel</label>" +
+    "                            <div class=\"col-sm-5\">" +
+    "                        <input type=\"text\" class=\"form-control\">" +
+    "                    </div>" +
+    "                    <label class=\"col-sm-5 control-label\">out_channel</label>" +
+    "                            <div class=\"col-sm-5\">" +
+    "                        <input type=\"text\" class=\"form-control\">" +
+    "                    </div>" +
+    "                </div>" +
+    "                </form>"
+
+var searh_layer = {
+    "view_layer": view_layer,
+    "linear_layer": linear_layer
+}
