0a7d10e189a1cb9a10069e6d13db11250300bb90
mahaoxiang822 <464365439@qq.com>
2019-05-20 22:31:05 +0800

    修复不能删除 去掉了统计 完善了下载功能

--- VisualPytorch/NeuralNetwork/permissions.py
+++ VisualPytorch/NeuralNetwork/permissions.py
@@ -1,7 +1,10 @@
+from NeuralNetwork.models import Network
+
 class ChangeModel(object):
 
     def has_permission(self, request,view):
-        if request.method == 'GET' or request.user.id == view.kwargs['pk']:
+        aquire_object = Network.objects.get(pk=view.kwargs['pk'])
+        if request.method == 'GET' or request.user.id == aquire_object.creator_id:
             return True
         else:
             return False
